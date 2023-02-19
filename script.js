6926f1edd9dd7418258e173701d9bf5bb663c6c9
yaromoon <104704933+yaromoon@users.noreply.github.com>
2023-02-19 19:28:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  
-    $('.block').slideToggle(60000)
-setTimeout(function(){document.querySelector(`.time`).innerHTML = "1";},60000)
-    $('.block').slideToggle(60000)
-    setTimeout(function(){document.querySelector(`.time`).innerHTML = "0";},60000)
-
-
-})
